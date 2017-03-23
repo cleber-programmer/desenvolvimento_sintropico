78316337d055f07a80db8f760b6ac4f1a77bbbac
cleber.goncalves <cleber.goncalves@concrete.com.br>
2017-03-23 18:32:02 -0300

    Refatoracao do codigo para melhorar a legibilidade do codigo

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,18 @@
-Rex(function ({ atom, radio }) {
+Rex(function ({ atom }) {
 
-  
+	@atom.component('x-x')
+	class X {
+
+		@atom.event('click', 'span')
+		action() {
+			console.log('kkkkk')
+		}
+
+		@atom.attached
+		render() {
+			this.innerHTML = '<span>ola</span>';
+		}
+
+	}
 
 });
