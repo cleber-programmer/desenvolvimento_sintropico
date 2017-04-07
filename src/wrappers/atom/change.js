779918d26f68fcb30a575dd7b486b85718d06762
cleber.goncalves <cleber.goncalves@concrete.com.br>
2017-04-07 14:00:33 -0300

    Documentacao do e padronizacao do codigo, para melhorar o entendimento

--- src/wrappers/atom/change.js
+++ src/wrappers/atom/change.js
@@ -1,18 +1,30 @@
 Rex(function ({ atom, contains, hook }) {
 
+  /**
+   * Extensao do modulo atom, o decorador change notifica o metodo
+   * decorado quando o atributo identificado for alterado
+   */
   Object.assign(atom, {
     change(...properties) {
-
       return function (target, prop, descriptor) {
 
-        hook.before(target, 'attributeChangedCallback', function (name, oldValue, newValue, predicate) {
-          contains(name, properties) && (this[prop](newValue, oldValue, name), predicate.call(this, name, oldValue, newValue));
-        });
+        /**
+         * Notifica o metodo que fora decorado, para receber o novo valor da
+         * propriedade alterado
+         */
+        function hookCallback(name, oldValue, newValue) {
+          contains(name, properties) && (this[prop](newValue, oldValue, name));
+        }
+
+        /**
+         * Altera o metodo attributeChangedCallback para incluir o hook, no momento
+         * que este metodo for chamado, sera repassado para o metodo que fora decorado
+         */
+        hook.before(target, 'attributeChangedCallback', hookCallback);
 
         return descriptor;
 
       };
-
     }
   });
 
--- src/wrappers/atom/component.js
+++ src/wrappers/atom/component.js
@@ -1,17 +1,23 @@
 Rex(function ({ atom }) {
 
+  /**
+   * Extensao do modulo atom, o decorador componente cria um componente
+   * como o nome da tag quer fora passado
+   */
 	Object.assign(atom, {
     component(tagName, extending) {
-
       return function (target) {
 
+        /**
+         * Cria um novo Web Components customizado, desde que o nome da tag tenha
+         * um hifen e que este nome ja nao foi registrado
+         */
       	document.registerElement(tagName, {
           prototype: Object.assign(Object.create(HTMLElement.prototype), target.prototype),
           [extending ? 'extends' : '']: extending
         });
 
       };
-
     }
   });
 
