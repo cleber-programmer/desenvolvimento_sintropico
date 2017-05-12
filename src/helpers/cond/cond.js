b33cce87f0ce82fdd5985a1556f0154f37d60832
cleber.programmer <cleber.programmer@hotmail.com>
2017-05-12 15:55:28 -0300

    Inclusao de documentacao no modulo cond

--- src/helpers/cond/cond.js
+++ src/helpers/cond/cond.js
@@ -1,12 +1,18 @@
 Rex('cond', function () {
 
+  /**
+   * Como uma cadeia de responsabilidade, passando consjuntos de predicado
+   * verificador e um predicado de resposata
+   */
   return function (...chain) {
     return function (...args) {
 
-      let index = -1;
-
-      while(++index < chain.length) {
-        if(chain[index][0](...args)) return chain[index][1](...args);
+      /**
+       * Anda pelas cadeias ate encontrar uma cadeia
+       * que se identifique
+       */
+      for (let index in chain) {
+        if (chain[index][0](...args)) return chain[index][1](...args);
       }
 
     };
