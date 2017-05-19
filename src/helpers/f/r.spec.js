d53d30003b01d9e6eb5aaec02fc904e3769a0194
cleber.programmer <cleber.programmer@hotmail.com>
2017-05-19 10:27:53 -0300

    refatoracao

--- src/helpers/f/r.spec.js
+++
@@ -1,9 +0,0 @@
-Rex(function (R) {
-
-  it('f', function (ok) {
-    ok(/false/.test(R.f()), 'Retorna falso');
-    ok(/false/.test(R.f(10)), 'Retorna falso mesmo passando um valor');
-    ok(/false/.test(R.f(true)), 'Retorna falso mesmo passando verdadeiro');
-  });
-
-});
